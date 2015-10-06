6ebbe9db08d1073a3bb1562e92274b3c168acf61
Nishant Totla <nishanttotla@gmail.com>
2015-10-06 15:11:44 -0700

    Show only excerpts on home page, reduce margin between post-meta and excerpt, and add date to post-meta

--- index.php
+++ index.php
@@ -23,26 +23,26 @@
 
 <header id="masthead" class="site-header" role="banner">
 	<div class="container">
-		
+
 		<div class="gravatar">
-			<?php 
+			<?php
 				// grab admin email and their photo
 				$admin_email = get_option('admin_email');
-				echo get_avatar( $admin_email, 100 ); 
+				echo get_avatar( $admin_email, 100 );
 			?>
 		</div><!--/ author -->
-		
+
 		<div id="brand">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
 		</div><!-- /brand -->
-	
+
 		<nav role="navigation" class="site-navigation main-navigation">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- .site-navigation .main-navigation -->
-		
+
 		<div class="clear"></div>
 	</div><!--/container -->
-		
+
 </header><!-- #masthead .site-header -->
 
 <div class="container">
@@ -55,45 +55,48 @@
 	/*-----------------------------------------------------------------------------------*/
 	/* Start Home loop
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	if( is_home() || is_archive() ) {
-	
+
 ?>
 			<?php if ( have_posts() ) : ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<article class="post">
-					
+
 						<h1 class="title">
 							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 								<?php the_title() ?>
 							</a>
 						</h1>
 						<div class="post-meta">
+							<span class="comments-link">
+								<?php the_date(); ?>
+							</span>
 							<?php if( comments_open() ) : ?>
 								<span class="comments-link">
-									<?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
+									| <?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
 								</span>
 							<?php endif; ?>
-						
+
 						</div><!--/post-meta -->
-						
+
 						<div class="the-content">
-							<?php the_content( 'Continue...' ); ?>
-							
+							<?php the_excerpt(); ?>
+
 							<?php wp_link_pages(); ?>
 						</div><!-- the-content -->
-						
+
 						<div class="meta clearfix">
 							<div class="category"><?php echo get_the_category_list(); ?></div>
 							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
 						</div><!-- Meta -->
-						
+
 					</article>
 
 				<?php endwhile; ?>
-				
+
 				<!-- pagintation -->
 				<div id="pagination" class="clearfix">
 					<div class="past-page"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
@@ -102,21 +105,21 @@
 
 
 			<?php else : ?>
-				
+
 				<article class="post error">
 					<h1 class="404">Nothing posted yet</h1>
 				</article>
 
 			<?php endif; ?>
 
-		
+
 	<?php } //end is_home(); ?>
 
 <?php
 	/*-----------------------------------------------------------------------------------*/
 	/* Start Single loop
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	if( is_single() ) {
 ?>
 
@@ -126,7 +129,7 @@
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<article class="post">
-					
+
 						<h1 class="title"><?php the_title() ?></h1>
 						<div class="post-meta">
 							<?php if( comments_open() ) : ?>
@@ -134,24 +137,24 @@
 									<?php comments_popup_link( __( 'Comment', 'less' ), __( '1 Comment', 'less' ), __( '% Comments', 'less' ) ); ?>
 								</span>
 							<?php endif; ?>
-						
+
 						</div><!--/post-meta -->
-						
+
 						<div class="the-content">
 							<?php the_content( 'Continue...' ); ?>
-							
+
 							<?php wp_link_pages(); ?>
 						</div><!-- the-content -->
-						
+
 						<div class="meta clearfix">
 							<div class="category"><?php echo get_the_category_list(); ?></div>
 							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
-						</div><!-- Meta -->						
-						
+						</div><!-- Meta -->
+
 					</article>
 
 				<?php endwhile; ?>
-				
+
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
 					if ( comments_open() || '0' != get_comments_number() )
@@ -160,7 +163,7 @@
 
 
 			<?php else : ?>
-				
+
 				<article class="post error">
 					<h1 class="404">Nothing posted yet</h1>
 				</article>
@@ -169,12 +172,12 @@
 
 
 	<?php } //end is_single(); ?>
-	
+
 <?php
 	/*-----------------------------------------------------------------------------------*/
 	/* Start Page loop
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	if( is_page()) {
 ?>
 
@@ -183,21 +186,21 @@
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<article class="post">
-					
+
 						<h1 class="title"><?php the_title() ?></h1>
-						
+
 						<div class="the-content">
 							<?php the_content(); ?>
-							
+
 							<?php wp_link_pages(); ?>
 						</div><!-- the-content -->
-						
+
 					</article>
 
 				<?php endwhile; ?>
 
 			<?php else : ?>
-				
+
 				<article class="post error">
 					<h1 class="404">Nothing posted yet</h1>
 				</article>
