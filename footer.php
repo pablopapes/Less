773a5e207dda0b9ea7769054af06fc2e7f5bdef7
Nishant Totla <nishanttotla@gmail.com>
2015-10-08 13:42:20 -0700

    Adding social icons to footer and creating separate footer file

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="site-footer" role="contentinfo">
+  <div class="site-info container">
+      <a target="_blank" href="https://twitter.com/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/twitter-4-32.png"/></a>
+      <a target="_blank" href="https://instagram.com/nishanttotla/"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/instagram-4-32.png"/></a>
+      <a target="_blank" href="https://www.facebook.com/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/facebook-4-32.png"/></a>
+      <a target="_blank" href="https://github.com/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/github-9-32.png"/></a>
+      <a target="_blank" href="https://www.linkedin.com/in/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/linkedin-4-32.png"/></a><br>
+    <?php do_action( 'break_credits' ); ?>
+    <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
+    <span class="sep"> and based on </span>
+    <a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
+  </div><!-- .site-info -->
+</footer><!-- #colophon .site-footer -->
--- index.php
+++ index.php
@@ -221,16 +221,8 @@
 	/*-----------------------------------------------------------------------------------*/
 	/* Start Footer
 	/*-----------------------------------------------------------------------------------*/
+	get_footer();
 ?>
-
-<footer class="site-footer" role="contentinfo">
-	<div class="site-info container">
-		<?php do_action( 'break_credits' ); ?>
-		<a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
-		<span class="sep"> and based on </span>
-		<a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
-	</div><!-- .site-info -->
-</footer><!-- #colophon .site-footer -->
 
 <?php wp_footer(); ?>
 
