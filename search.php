57bd866703179393000e3662838cbf8774c35d5d
Nishant Totla <nishanttotla@gmail.com>
2015-10-08 01:16:35 -0700

    Deleting redundant code from search template

--- search.php
+++ search.php
@@ -56,7 +56,7 @@
       <h2>Search results: <?php echo $total_results ?></h2>
 <?php
   /*-----------------------------------------------------------------------------------*/
-  /* Start Home loop
+  /* Start Search loop
   /*-----------------------------------------------------------------------------------*/
 
   if( is_search() ) { // redundant check
@@ -109,104 +109,7 @@
       <?php endif; ?>
 
 
-  <?php } //end is_home(); ?>
-
-<?php
-  /*-----------------------------------------------------------------------------------*/
-  /* Start Single loop
-  /*-----------------------------------------------------------------------------------*/
-
-  if( is_single() ) {
-?>
-
-
-      <?php if ( have_posts() ) : ?>
-
-        <?php while ( have_posts() ) : the_post(); ?>
-
-          <article class="post">
-
-            <h1 class="title"><?php the_title() ?></h1>
-            <div class="post-meta">
-              <span class="comments-link">
-                <?php the_date(); ?>
-              </span>
-              <?php if( comments_open() ) : ?>
-                <span class="comments-link">
-                  | <?php comments_popup_link( __( 'Comment', 'less' ), __( '1 Comment', 'less' ), __( '% Comments', 'less' ) ); ?>
-                </span>
-              <?php endif; ?>
-
-            </div><!--/post-meta -->
-
-            <div class="the-content">
-              <?php the_content( 'Continue...' ); ?>
-
-              <?php wp_link_pages(); ?>
-            </div><!-- the-content -->
-
-            <div class="meta clearfix">
-              <div class="category"><?php echo get_the_category_list(); ?></div>
-              <div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
-            </div><!-- Meta -->
-
-          </article>
-
-        <?php endwhile; ?>
-
-        <?php
-          // If comments are open or we have at least one comment, load up the comment template
-          if ( comments_open() || '0' != get_comments_number() )
-            comments_template( '', true );
-        ?>
-
-
-      <?php else : ?>
-
-        <article class="post error">
-          <h2 class="404">Nothing posted yet</h2>
-        </article>
-
-      <?php endif; ?>
-
-
-  <?php } //end is_single(); ?>
-
-<?php
-  /*-----------------------------------------------------------------------------------*/
-  /* Start Page loop
-  /*-----------------------------------------------------------------------------------*/
-
-  if( is_page()) {
-?>
-
-      <?php if ( have_posts() ) : ?>
-
-        <?php while ( have_posts() ) : the_post(); ?>
-
-          <article class="post">
-
-            <h1 class="title"><?php the_title() ?></h1>
-
-            <div class="the-content">
-              <?php the_content(); ?>
-
-              <?php wp_link_pages(); ?>
-            </div><!-- the-content -->
-
-          </article>
-
-        <?php endwhile; ?>
-
-      <?php else : ?>
-
-        <article class="post error">
-          <h2 class="404">Nothing posted yet</h2>
-        </article>
-
-      <?php endif; ?>
-
-  <?php } // end is_page(); ?>
+  <?php } //end is_search(); ?>
 
     </div><!-- #content .site-content -->
   </div><!-- #primary .content-area -->
