f51c2be9ba0cc8ae79716720392d8ab8a34a489a
Nishant Totla <nishanttotla@gmail.com>
2015-10-14 02:12:17 -0700

    minor changes

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
       <a target="_blank" href="https://github.com/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/github-9-32.png"/></a>
       <a target="_blank" href="https://www.linkedin.com/in/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/linkedin-4-32.png"/></a><br>
     <?php do_action( 'break_credits' ); ?>
-    <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
+    <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Powered by WordPress</a>
     <span class="sep"> and based on </span>
     <a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
   </div><!-- .site-info -->
