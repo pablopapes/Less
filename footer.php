d4cea0266a16da487fa4f32588dae553335e65d6
Nishant Totla <nishanttotla@gmail.com>
2015-10-22 01:19:49 -0700

    Style changes and updating footer links

--- footer.php
+++ footer.php
@@ -4,7 +4,9 @@
       <a target="_blank" href="https://instagram.com/nishanttotla/"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/instagram-4-32.png"/></a>
       <a target="_blank" href="https://www.facebook.com/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/facebook-4-32.png"/></a>
       <a target="_blank" href="https://github.com/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/github-9-32.png"/></a>
-      <a target="_blank" href="https://www.linkedin.com/in/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/linkedin-4-32.png"/></a><br>
+      <a target="_blank" href="https://www.linkedin.com/in/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/linkedin-4-32.png"/></a>
+      <a target="_blank" href="http://www.nishanttotla.com/blog/feed/"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/rss-4-32.png"/></a>
+      <a target="_blank" href="https://github.com/nishanttotla/Less"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/code-32.png"/></a><br>
     <?php do_action( 'break_credits' ); ?>
     <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Powered by WordPress</a>
     <span class="sep"> and based on </span>
