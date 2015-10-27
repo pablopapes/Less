c848b34a76b6d1e75de41df7e1c7700fccdfa087
Nishant Totla <nishanttotla@gmail.com>
2015-10-27 12:29:18 -0700

    Minor changes

--- footer.php
+++ footer.php
@@ -8,8 +8,8 @@
       <a target="_blank" href="http://www.nishanttotla.com/blog/feed/"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/rss-4-32.png"/></a>
       <a target="_blank" href="https://github.com/nishanttotla/Less"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/code-32.png"/></a><br>
     <?php do_action( 'break_credits' ); ?>
-    <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Powered by WordPress</a>
+    <a href="http://wordpress.org/" target="_blank" title="A Semantic Personal Publishing Platform" rel="generator">Powered by WordPress</a>
     <span class="sep"> and based on </span>
-    <a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
+    <a href="http://lessmade.com/themes/less" target="_blank" rel="theme">LESS</a>
   </div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
