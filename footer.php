a32e90680d00a66168535d4791c3cb8fade0c194
Pablo Papes <pablopapes@gmail.com>
2019-03-19 18:34:34 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,15 +1,7 @@
 <footer class="site-footer" role="contentinfo">
   <div class="site-info container">
-      <a target="_blank" href="https://twitter.com/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/twitter-4-32.png"/></a>
-      <a target="_blank" href="https://instagram.com/nishanttotla/"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/instagram-4-32.png"/></a>
-      <a target="_blank" href="https://www.facebook.com/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/facebook-4-32.png"/></a>
-      <a target="_blank" href="https://github.com/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/github-9-32.png"/></a>
-      <a target="_blank" href="https://www.linkedin.com/in/nishanttotla"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/linkedin-4-32.png"/></a>
-      <a target="_blank" href="http://www.nishanttotla.com/blog/feed/"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/rss-4-32.png"/></a>
-      <a target="_blank" href="https://github.com/nishanttotla/Less"><img src="http://www.nishanttotla.com/blog/wp-content/uploads/2015/10/code-32.png"/></a><br>
-    <?php do_action( 'break_credits' ); ?>
-    <a href="http://wordpress.org/" target="_blank" title="A Semantic Personal Publishing Platform" rel="generator">Powered by WordPress</a>
-    <span class="sep"> and based on </span>
-    <a href="http://lessmade.com/themes/less" target="_blank" rel="theme">LESS</a>
+     <?php do_action( 'break_credits' ); ?>
+    Made with ♥ in  
+    <a href="https://es.wikipedia.org/wiki/Provincia_de_Entre_R%C3%ADos" target="_blank" title="Entre Ríos" rel="generator">Entre Ríos</a>
   </div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
